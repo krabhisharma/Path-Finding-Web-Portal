35fb06ae8e0d337e2da52eb7c852574f67588055
ksjo <kyle_sjostrand@starkey.com>
2020-10-07 23:34:15 -0500

    Adds a 404 route and component.

---
+++ src/components/pages/NoPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+export default NoPage => {
+    return (
+        <div className='noPage'>
+            <h1 className='heading-lg'>This is the path less traveled.</h1>
+            <a className='link' href='/' title='Go Home' >Go Home</a>
+        </div>
+    )
+}
+
